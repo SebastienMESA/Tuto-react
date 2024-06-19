6081d13dd1cd83d2e2460a5b81241c6da96a6d87
Sebastien MESA <mesasebastien@outlook.fr>
2024-06-19 19:34:19 +0200

    feat: Ajout du board, ajout du composant square, ajout du clickevent sur chaque square

--- tuto-react/src/App.js
+++ tuto-react/src/App.js
@@ -1,26 +1,42 @@
 import { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  function handleClick() {
-    setCount(count + 1);
-  }
-
+function Square({value, onSquareClick}) {
   return (
-    <div>
-      <h1>Bienvenue dans mon appli</h1>
-      <MyButton count={count} onClick={handleClick} />
-      <MyButton count={count} onClick={handleClick}/>
-    </div>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
   );
 }
 
-const MyButton = ({ count, onClick }) => {
+
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
 
   return (
-    <button onClick={onClick}>Cliqué {count} fois</button>
-  )
+  <>
+  <div className="board-row">
+    <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+    <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+    <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+  </div>
+  <div className="board-row">
+    <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+    <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+    <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+  </div>
+  <div className="board-row">
+    <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+    <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+    <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+  </div>
+  </>
+);
 }
 
-
-export default App;
